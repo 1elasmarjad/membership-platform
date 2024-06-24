f9381d6b9416eb20ba0576b2ee1b615d3ec407ac
1elasmarjad <42979241+1elasmarjad@users.noreply.github.com>
2024-06-24 11:35:14 -0400

    🎞️form action for creating merchant data

--- src/app/_components/merchant-form.tsx
+++ src/app/_components/merchant-form.tsx
@@ -4,8 +4,14 @@ import { createAllMerchantData } from "~/server/actions";
 
 export default function MerchantForm() {
   return (
-    <form action={createAllMerchantData}>
-      <input type="text" placeholder="Merchant Name" />
+    <form action={createAllMerchantData} className="flex flex-col gap-6">
+      <label className="flex flex-col">
+        Name
+        <input type="text" id="name" name="name" />
+      </label>
+
+        
+
       <button type="submit">Create Merchant</button>
     </form>
   );
--- src/server/actions.ts
+++ src/server/actions.ts
@@ -1,8 +1,36 @@
 "use server";
 
+import { getSession } from "next-auth/react";
 import "server-only";
+import { createMerchant } from "./lib/merchant";
 
-export async function createAllMerchantData(formdata: FormData) {
-  console.log(formdata);
-  return null;
+export async function createAllMerchantData(formData: FormData) {
+  const data: Record<string, unknown> = {};
+  formData.forEach((value, key) => (data[key] = value));
+  const json = JSON.stringify(data);
+
+  console.log(json);
+
+  // zod validation required
+
+  // if (!formData.get("name")) {
+  //   throw Error("Invalid form input");
+  // }
+
+  // // get user
+  // const session = await getSession();
+
+  // if (!session) {
+  //   throw Error("Not authenticated");
+  // }
+
+  // const userId = session.user.id;
+
+  // // create merchant
+  // await createMerchant({
+  //   name: formData.get("name"),
+  //   ownerId: userId,
+  // });
+
+  // return null;
 }
