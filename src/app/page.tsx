c43aa84c6d61334d5238b1ef7bae10b49892cd3e
1elasmarjad <jadelasmar4@gmail.com>
2024-07-05 23:52:09 -0400

    🏠 started landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,31 @@
 import Link from "next/link";
 
-import { getServerAuthSession } from "~/server/auth";
-
 export default async function Home() {
-  const session = await getServerAuthSession();
-
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+    <main className="flex min-h-screen w-full flex-col">
+      <div className="flex flex-col items-center">
+        <h1 className="mt-12 text-center text-6xl font-semibold">
+          Monetize Your Community<b className="text-blue-500">.</b>
         </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        <p className="mt-3 text-center text-2xl">
+          Building bridges between fans and creators
+        </p>
+
+        <div className="flex justify-between w-full max-w-md">
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+            href="/create"
+            className="mt-6 w-fit rounded-lg bg-blue-500 px-12 py-3 text-lg text-white transition-all hover:bg-blue-400"
           >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
+            Register Now
           </Link>
+
           <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
+            href="/find"
+            className="mt-6 w-fit rounded-lg bg-blue-500 px-12 py-3 text-lg text-white transition-all hover:bg-blue-400"
           >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
+            Find Creators
           </Link>
         </div>
-        <div className="flex flex-col items-center gap-2">
-
-          <div className="flex flex-col items-center justify-center gap-4">
-            <p className="text-center text-2xl text-white">
-              {session && <span>Logged in as {session.user?.name}</span>}
-            </p>
-            <Link
-              href={session ? "/api/auth/signout" : "/api/auth/signin"}
-              className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-            >
-              {session ? "Sign out" : "Sign in"}
-            </Link>
-          </div>
-        </div>
-
       </div>
     </main>
   );
